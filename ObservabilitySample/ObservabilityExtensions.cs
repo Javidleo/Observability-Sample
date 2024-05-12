548ac7f300bda47acf8bba0dab8c1d48ee5b24ae
javidleo <javidleo.ef@gmail.com>
2024-05-12 23:36:56 +0330

    adding first commit

---
+++ ObservabilitySample/ObservabilityExtensions.cs
@@ -0,0 +1,85 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Routing;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using OpenTelemetry.Logs;
+using OpenTelemetry.Metrics;
+using OpenTelemetry.Resources;
+using OpenTelemetry.Trace;
+
+namespace ObservabilitySample;
+
+public static class ObservabilityExtensions 
+{
+    public static IServiceCollection AddObservabilityDefaults(this IServiceCollection services, IWebHostEnvironment env, string serviceName = "test-service")
+    {
+        services.AddOpenTelemetry()
+            .WithMetrics(metrics =>
+            {
+                metrics.AddRuntimeInstrumentation()
+                       .AddProcessInstrumentation()
+                       .AddBuiltInMeters()
+                       .AddConsoleExporter()
+                       .AddPrometheusExporter(); // this extension method set up /metrics endpoint and expose all metrics on that for service
+                                                 // as prometheus is a pull mode service(it fetchs data from the host)
+                                                 // you can find this service configurations on prometheus configs in devops folder.
+            })
+            .WithTracing(tracing =>
+            {
+                if (env.IsDevelopment())
+                {
+                    // We want to view all traces in development
+                    tracing.SetSampler(new AlwaysOnSampler());
+                }
+                // adding your event provider service activity source name.
+                // create an activity 
+                tracing.AddSource("Rabbit-MQ");
+                tracing.SetResourceBuilder(ResourceBuilder.CreateDefault().AddService(serviceName, serviceVersion: "1.0.0"));
+
+                tracing.AddAspNetCoreInstrumentation(i =>
+                {
+                    i.Filter = (httpContext) =>
+                    {
+                        var path = httpContext.Request.Path.Value;
+
+                        List<string> defaultIgnoreCases = ["/hc", "/metrics"];
+                        foreach (var ignoreCase in defaultIgnoreCases)
+                        {
+                            if (path!.StartsWith(ignoreCase, StringComparison.OrdinalIgnoreCase))
+                                return false;
+                        }
+
+                        return true;
+                    };
+                })
+                .AddHttpClientInstrumentation()
+                .AddOtlpExporter(i =>
+                {
+                    // only working with docker compose.
+                    i.Endpoint = new Uri("http://collector:4317");
+                });
+            });
+
+        return services;
+    }
+    public static WebApplication MapObservabilityEndpoints(this WebApplication app)
+    {
+        app.MapPrometheusScrapingEndpoint();
+        return app;
+    }
+    public static IEndpointRouteBuilder MapObservabilityEndpoints(this IEndpointRouteBuilder app)
+    {
+        app.MapPrometheusScrapingEndpoint();
+
+        return app;
+    }
+
+    private static MeterProviderBuilder AddBuiltInMeters(this MeterProviderBuilder meterProviderBuilder) =>
+        meterProviderBuilder.AddMeter(
+            "Microsoft.AspNetCore.Hosting",
+            "Microsoft.AspNetCore.Server.Kestrel",
+            "System.Net.Http");
+}
---
+++ ObservabilitySample/RabbitMqObservabilityFeatures.cs
@@ -0,0 +1,106 @@
+﻿using OpenTelemetry.Context.Propagation;
+using OpenTelemetry;
+using System.Diagnostics;
+using RabbitMQ.Client;
+using System.Text;
+using OpenTelemetry.Trace;
+using RabbitMQ.Client.Events;
+
+namespace ObservabilitySample;
+
+public class RabbitMqObservabilityFeatures
+{
+    private static ActivitySource _activity = new("Rabbit-MQ", "1.0.0");
+    private static readonly TextMapPropagator _propagator = Propagators.DefaultTextMapPropagator;
+
+    private static IEnumerable<string> ExtractTraceContextFromBasicProperties(IBasicProperties props, string key)
+    {
+        try
+        {
+            if (props.Headers.TryGetValue(key, out var value))
+            {
+                var bytes = value as byte[];
+                return [Encoding.UTF8.GetString(bytes)];
+            }
+        }
+
+        catch (Exception)
+        {
+            throw;
+        }
+
+        return [];
+    }
+
+
+    private async Task PublishEvent()
+    {
+        using var activity = _activity?.StartActivity("RabbitMq Publish", ActivityKind.Producer);
+
+        // Call AddActivityToHeaders() and add headers to Event.
+        // which you should catch them in the reciveEvent
+        // publish event through rabbit mq or masstransit
+    }
+    private async Task RecieveEvent(object sender, BasicDeliverEventArgs eventArgs)
+    {
+        var parentContext = _propagator.Extract(default, eventArgs.BasicProperties, ExtractTraceContextFromBasicProperties);
+
+        //Start Existing Activity Left From Event Publisher
+        using var activity = _activity?.StartActivity("Process Message", ActivityKind.Consumer, parentContext.ActivityContext);
+
+        string eventName = eventArgs.RoutingKey;
+        string message = Encoding.UTF8.GetString(eventArgs.Body.Span);
+        // adding headers comming from event to Activity
+        AddActivityToHeader(activity!, eventName);
+        try
+        {
+            // processing events.
+        }
+        catch (Exception ex)
+        {
+            //_logger.LogError(ex, message: ex.Message);
+            // catching exceptions in event consumers.
+            
+            activity.SetStatus(ActivityStatusCode.Error);
+            // store the error details in span information
+            activity.RecordException(ex);
+        }
+    }
+
+
+    /// <summary>
+    /// Activity Headers should be added in 
+    /// </summary>
+    /// <param name="activity"></param>
+    /// <param name="eventName"></param>
+    /// <param name="props"></param>
+    private void AddActivityToHeader(Activity activity, string eventName, IBasicProperties props = null!)
+    {
+        if (activity is null)
+            _activity = new ActivitySource("Rabbit-MQ", "1.0.0");
+
+        if (activity is not null)
+        {
+            _propagator.Inject(new PropagationContext(activity.Context, Baggage.Current), props, InjectContextIntoHeader);
+            activity?.SetTag("messaging.system", "rabbitmq");
+            activity?.SetTag("messaging.destination_kind", "queue");
+            activity?.SetTag("messaging.rabbitmq.event", eventName);
+        }
+        else
+        {
+            // throwing or logging error.
+        }
+    }
+    private void InjectContextIntoHeader(IBasicProperties props, string key, string value)
+    {
+        try
+        {
+            props.Headers ??= new Dictionary<string, object>();
+            props.Headers[key] = value;
+        }
+        catch (Exception ex)
+        {
+            // throwing or logging error.
+        }
+    }
+}
